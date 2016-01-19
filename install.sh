e8ffdd1623769b64d8c0cc5627b1f5f913a2628c
Jason Kridner <jkridner@beagleboard.org>
2016-01-19 20:39:24 +0000

    Add more install notes

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 # Invoke as beagleboard.org/install.sh (from one directory up).
 # It is assumed you have already run the following command to fetch code:
-# git clone http://www.beagleboard.org/beagleboard.org.git/ beagleboard.org
+# git clone https://github.com/beagleboard/beagleboard-org.git beagleboard.org
 # 
 # sudo apt-get update
 # sudo apt-get install -y default-jre
 # sudo apt-get install -y nginx
+# sudo apt-get install -y buildbot
+#
+# Debian files go in /mnt2/debian for debian.beagleboard.org
+#
+# sudo cp beagleboard.org/scripts/sites-enabled-default /etc/nginx/sites-enabled/default
+# sudo /etc/init.d/nginx start
+# crontab beagleboard.org/scripts/crontab
+#
 [ -d beagleboard.org ] || (echo "Run one directory above beagleboard.org" && exit -1)
 [ -e helma-1.6.1.tar.gz ] || wget http://adele.helma.org/download/helma/1.6.1/helma-1.6.1.tar.gz
 [ -e helma-1.6.1 ] || tar xvzf helma-1.6.1.tar.gz
