e71e3091057ab98242c3180f2a9245ddbc9540e7
Jason Kridner <jkridner@beagleboard.org>
2018-05-07 16:12:42 +0000

    No trust for adroll

--- static/beagleboard.js
+++ static/beagleboard.js
@@ -45,19 +45,3 @@ function goTracked(action, link, label, category) {
     }
 }
 
-// adroll
-adroll_adv_id = "EC3AD5CVBRBDZK2IF5HUG7";
-adroll_pix_id = "KPOF2PWBSVAU7LE7IYHV7U";
-(function () {
-var oldonload = window.onload;
-window.onload = function(){
-   __adroll_loaded=true;
-   var scr = document.createElement("script");
-   var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
-   scr.setAttribute('async', 'true');
-   scr.type = "text/javascript";
-   scr.src = host + "/j/roundtrip.js";
-   ((document.getElementsByTagName('head') || [null])[0] ||
-    document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
-   if(oldonload){oldonload()}};
-}());
