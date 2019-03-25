8316890e99af7ea25b7c0beb424bf1834fec4362
Jason Kridner <jkridner@beagleboard.org>
2019-03-25 16:49:31 +0000

    Update how wordpress is backed up

--- scripts/run_server.sh
+++ scripts/run_server.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 cd /home/ubuntu/beagleboard.org
-curl 'https://beagleboard.org/blog/wp-admin/export.php?content=all&download=true' > beagleboardorg.wordpress.xml
+#curl 'https://beagleboard.org/blog/wp-admin/export.php?content=all&download=true' > beagleboardorg.wordpress.xml
 git add beagleboardorg.wordpress.xml
 git commit --message="Blog updates" --author="Anonymous <anonymous@beagleboard.org>" beagleboardorg.wordpress.xml
+git add wp-uploads
+git commit --message="Blog images" --author="Anonymous <anonymous@beagleboard.org>" wp-uploads
 killall java
 killall -9 java
 git pull
