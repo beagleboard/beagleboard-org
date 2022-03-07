6847eb5f387692cde77e150a801cd2c2a370e2d0
Jason Kridner <jkridner@beagleboard.org>
2022-03-07 16:49:41 +0000

    latest gitlab

--- scripts/docker/gitlab-backup.sh
+++ scripts/docker/gitlab-backup.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
 docker exec gitlab_web_1 gitlab-backup create
+sudo cp /mnt/5tb/gitlab/config/gitlab* /home/ubuntu/workspace/beagleboard-docs/docker/gitlab/
+sudo chown ubuntu:ubuntu /home/ubuntu/workspace/beagleboard-docs/docker/gitlab/*
+cd /home/ubuntu/workspace/beagleboard-docs/docker/gitlab/
+git commit . -m "latest gitlab config"
+git push
